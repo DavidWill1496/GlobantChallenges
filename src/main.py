9c0efd469d8a6dc372e4bffb390b5f8bdcd761e5
David Galvis <56653514+DavidWill1496@users.noreply.github.com>
2023-03-03 14:30:33 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,12 +10,8 @@ from pandasql import sqldf
 
 app = Flask(__name__)
 
-#key_path = "env/BigQuery/challengeglobant-7559939a117e.json"
 project_id = "challengeglobant"
 dataset = "DataSet_Challenge"
-#credentials = service_account.Credentials.from_service_account_file(
-#    key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],)
-#client = bigquery.Client(credentials=credentials, project=project_id)
 client = bigquery.Client()
 
 # Data rules for table departments
